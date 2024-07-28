3241e1a9e7d56dfa5a9154a18acef30f23f9f130
Lucas Moura Alcantara <lucasmoura404@gmail.com>
2024-07-28 19:40:54 -0300

    projeto final

---
+++ santander-final/src/main/java/me/santander_final/SantanderFinalApplication.java
@@ -0,0 +1,13 @@
+package me.santander_final;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderFinalApplication.class, args);
+	}
+
+}
