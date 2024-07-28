fcabe58dc5c6ae32da80fc96b43e324401473ec4
Lucas Moura Alcantara <lucasmoura404@gmail.com>
2024-07-28 19:54:43 -0300

    final

---
+++ santander-final/src/main/java/me/santander_final/domain/model/Feature.java
@@ -0,0 +1,8 @@
+package me.santander_final.domain.model;
+
+import jakarta.persistence.Entity;
+
+@Entity(name = "tb_feature")
+public class Feature extends BaseItem {
+
+}
---
+++ santander-final/src/main/java/me/santander_final/domain/model/News.java
@@ -0,0 +1,8 @@
+package me.santander_final.domain.model;
+
+import jakarta.persistence.Entity;
+
+@Entity(name = "tb_news")
+public class News extends BaseItem {
+
+}
